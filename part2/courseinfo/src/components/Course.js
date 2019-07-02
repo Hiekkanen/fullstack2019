c991f8f3759b51c243ec5f0ac1ce813b25864e68
Oskar Hiekkanen <oskar.hiekkanen@gmail.com>
2019-07-02 13:40:57 +0300

    add phonebook

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,62 +1,56 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 
 
 const Course = (props) => {
   console.log('Course works')
-    console.log(props)
-    const { course } = props
+  console.log(props)
+  const { course } = props
+  //const totalAmount = course.parts.reduce((sum, part) => sum + part.exercises, 0)
 
+  return (
+    <div key={props.id}>
+      <div><Header course={course} /></div>
+      <div><Content parts={course.parts} /></div>
+      <div><Total course={course} /></div>
+    </div>
+  )
 
-    const totalAmount = course.parts.reduce((sum, part) =>  sum + part.exercises, 0)
-   
-
-
-    return (
-      <div>
-      <Header course={course} />
-      <Content parts={course.parts}/>
-      Total is {totalAmount}
-      </div>
-      
-
-
-
-    )
-  
 }
-  const Header = (props) => {
-    console.log('Header works')
-    console.log(props)
-    return (
-      <h1>{props.course.name}</h1>
-    )
-  }
 
-  const Content = (props) => {
-    console.log('content works')
-    console.log(props)
-    return (
-      <div>
-        {props.parts.map((part, i) => <Part part={part}/>)}
-      </div>
-    )
-  }
+const Header = (props) => {
+  console.log('Header works')
+  console.log(props)
+  return (
+    <h1>{props.course.name}</h1>
+  )
+}
+
+const Content = (props) => {
+  console.log('content works')
+  console.log(props)
+  return (
+    <div>
+      {props.parts.map(part => <Part part={part} key={part.id} />)}
+    </div>
+  )
+}
+
+const Part = (props) => {
+  console.log(props)
   
-  const Part = (props) => {
-    console.log(props)
-    return (
-      <div>
-          {props.part.name} {props.part.exercises}
-      </div>
-    )
-  }
+  return (
+    <div key={props.id}>
+      {props.part.name} {props.part.exercises} 
+    </div>
+  )
+}
 
+const Total = (props) => {
+  const course = props.course
+  const totalAmount = course.parts.reduce((sum, part) => sum + part.exercises, 0)
+  return <b>total of {totalAmount} exercises</b>
+}
 
-  
-
-
-  
 
 
 export default Course;
--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -2,43 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-/*
-const Header = (props) => {
-  console.log(props)
-  return (
-    <h1>{props.course}</h1>
-  )
-}
-
-const Content = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <Part part={props.parts[0]} />
-      <Part part={props.parts[1]} />
-      <Part part={props.parts[2]} />
-    </div>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <div>
-      <p>
-        {props.part.name} {props.part.exercises}
-      </p>
-    </div>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <div>
-      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
-    </div>
-  )
-}
-*/
-
 
 ReactDOM.render(<App/>, document.getElementById('root'))
