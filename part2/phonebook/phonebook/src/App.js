731a7cf5baa78d8c5ed7bda0fd6c9aa810428750
Oskar Hiekkanen <oskar.hiekkanen@gmail.com>
2019-07-02 17:25:14 +0300

    2.9 done

--- part2/phonebook/phonebook/src/App.js
+++ part2/phonebook/phonebook/src/App.js
@@ -2,47 +2,85 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
+
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [findName, setFindName] = useState('')
+  const [displayRows, setDisplayRows] = useState(persons)
+
 
   const addName = (event) => {
     event.preventDefault()
     console.log('button clicked', event.target)
 
     //check if person is in list
+
     if (isDuplicate(newName)) {
       window.alert(`${newName} is already added to phonebook`)
     }
+
     else {
-      setPersons(persons.concat([{ name: newName }]))
+      setPersons(persons.concat([{ name: newName, number: newNumber }]))
       console.log("persons: ", persons)
-      setNewName("")
+
     }
   }
 
+  const setQuery = (query) => {
+    let p = persons.filter(person => person.name.toLowerCase().includes(query.toLowerCase()))
+    setDisplayRows(p)
+    console.log(displayRows)
+  }
+
+
+
   const isDuplicate = (name) => {
     let p = persons.filter(person => person.name === name)
+    console.log(p)
     return p.length > 0
   }
 
 
   const handleNameChange = (event) => {
-    console.log(event.target.value)
     setNewName(event.target.value)
   }
 
-  const rows = () => persons.map(person => <li key={person.name}>{person.name}</li>)
+  const handleNumberChange = (event) => {
+    console.log("event number", event.target.value)
+    setNewNumber(event.target.value)
+  }
+
+  const handleFinderChange = (event) => {
+    console.log("query", event.target.value)
+    setQuery(event.target.value)
+    setFindName(event.target.value)
+  }
+
+
+  const rows = () => displayRows.map(person => <li key={person.name}>{person.name} {person.number} </li>)
+
+
 
 
 
 
   return (
     <div>
+      <form onSubmit={addName}>
+        Filter shown with: <input value={findName} onChange={handleFinderChange} />
+      </form>
+
       <h2>Phonebook</h2>
       <form onSubmit={addName}>
         name: <input value={newName} onChange={handleNameChange} />
-        <div>          <button type="submit">add</button>
+        number: <input value={newNumber} onChange={handleNumberChange} />
+        <div>
+          <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
