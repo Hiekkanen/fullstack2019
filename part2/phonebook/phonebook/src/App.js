2b9495f3f2b92d0861e2578f1786cc58d810fd74
Oskar Hiekkanen <oskar.hiekkanen@gmail.com>
2019-07-02 15:19:51 +0300

    2.7 done

--- part2/phonebook/phonebook/src/App.js
+++ part2/phonebook/phonebook/src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+
+    //check if person is in list
+    if (isDuplicate(newName)) {
+      window.alert(`${newName} is already added to phonebook`)
+    }
+    else {
+      setPersons(persons.concat([{ name: newName }]))
+      console.log("persons: ", persons)
+      setNewName("")
+    }
+  }
+
+  const isDuplicate = (name) => {
+    let p = persons.filter(person => person.name === name)
+    return p.length > 0
+  }
+
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const rows = () => persons.map(person => <li key={person.name}>{person.name}</li>)
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        name: <input value={newName} onChange={handleNameChange} />
+        <div>          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {rows()}
+      </ul>
+
     </div>
-  );
+  )
 }
 
-export default App;
+
+
+export default App
