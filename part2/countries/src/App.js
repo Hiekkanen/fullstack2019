2e3fec7f174f0e572848caaa37e0ee6ec42f2e24
Oskar Hiekkanen <oskar.hiekkanen@gmail.com>
2019-07-16 12:23:16 +0300

    dev branch for countries

---
+++ part2/countries/src/App.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import axios from 'axios'
+import Country from "./components/Country.js"
+
+
+const url = "https://restcountries.eu/rest/v2/all"
+
+
+const App = () => {
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get(url)
+      .then(response => {
+        console.log('promise fulfilled')
+        setCountries(response.data)
+      })
+  }, [])
+
+
+  console.log("countries:",  countries)
+
+
+  return (
+    <div>
+
+      <Country country={ countries[1] } />
+
+    </div>
+  )
+}
+
+
+/*
+
+  <div>
+      <div>
+        <ul>
+          {countries.map(c => <li key={c.name}> {c.name} </li>)}
+        </ul>
+      </div>
+      <div>
+      
+*/
+
+
+
+
+export default App;
---
+++ part2/countries/src/components/Country.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const Country = (props) => {
+    const country = props.country
+    console.log(country)
+    return (
+        <div>
+            <h1>name: {country.name}</h1>
+            {console.log(country.name)}
+            capital {country.capital}
+            population: {country.population}
+        </div>
+    )
+}
+
+export default Country
